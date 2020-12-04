d830db1289e4016be64479a2d89b310074af4395
woojiahao <woojiahao1234@gmail.com>
2020-12-05 00:20:17 +0800

    Add day 1 part 2 solution

--- day-1/solution.exs
+++ day-1/solution.exs
@@ -1,12 +1,23 @@
 defmodule Solution do 
-  defp combinations(elems), do: for x <- elems, y <- elems, x != y, do: [x, y]
-  def partOne() do
-    File.read!("data.txt") 
+  defp combinations(elems, size) when size == 2, do: for x <- elems, y <- elems, x != y, do: [x, y]
+  defp combinations(elems, size) when size == 3, do: for x <- elems, y <- elems, z <- elems, x != y, y != z, x != z, do: [x, y, z]
+  defp data(size \\ 2) do
+    File.read!("data.txt")
       |> String.split("\n", trim: true)
       |> Enum.map(&String.to_integer/1)
-      |> combinations
-      |> Enum.filter(fn x -> List.first(x) + List.last(x) == 2020 end)
-      |> List.first
+      |> combinations(size)
+  end 
+  defp acc(lst) do
+    lst 
+      |> List.first 
       |> List.foldl(1, fn acc, x -> x * acc end)
+  end
+
+  def partOne() do
+    data() |> Enum.filter(fn [x, y] -> x + y == 2020 end) |> acc
+  end
+
+  def partTwo() do
+    data(3) |> Enum.filter(fn [x, y, z] -> x + y + z == 2020 end) |> acc
   end
 end
