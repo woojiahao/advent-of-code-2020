06fe371fef89d4e005515d8c095df05355896948
woojiahao <woojiahao1234@gmail.com>
2020-12-05 11:45:15 +0800

    Add solution for day 2 part 1

--- day-2/solution.exs
+++ day-2/solution.exs
@@ -1,7 +1,17 @@
 defmodule Solution do
-  defp solve() do
+  defp validate(input) do
+    [min, max, letter, password] = Regex.run(
+      ~r/^(\d+)-(\d+) (\w): (\w+)$/,
+      input,
+      capture: :all_but_first
+    )
+    freq = password |> String.graphemes() |> Enum.frequencies() |> Map.get(letter)
+    freq >= String.to_integer(min) and freq <= String.to_integer(max) 
   end
 
-  def partOne(), do: solve()
-  def partTwo(), do: solve()
+  def partOne() do
+    File.read!("data.txt") 
+      |> String.split("\n", trim: true) 
+      |> Enum.count(&validate/1) 
+  end
 end
