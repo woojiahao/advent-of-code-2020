c928f4d5e19f582eb5ed6b451d9c3042821ea415
woojiahao <woojiahao1234@gmail.com>
2020-12-05 11:46:19 +0800

    Refactor day 2 part 1 solution

--- day-2/solution.exs
+++ day-2/solution.exs
@@ -1,17 +1,9 @@
 defmodule Solution do
   defp validate(input) do
-    [min, max, letter, password] = Regex.run(
-      ~r/^(\d+)-(\d+) (\w): (\w+)$/,
-      input,
-      capture: :all_but_first
-    )
+    [min, max, letter, password] = Regex.run(~r/^(\d+)-(\d+) (\w): (\w+)$/, input, capture: :all_but_first)
     freq = password |> String.graphemes() |> Enum.frequencies() |> Map.get(letter)
     freq >= String.to_integer(min) and freq <= String.to_integer(max) 
   end
 
-  def partOne() do
-    File.read!("data.txt") 
-      |> String.split("\n", trim: true) 
-      |> Enum.count(&validate/1) 
-  end
+  def partOne(), do: File.read!("data.txt") |> String.split("\n", trim: true) |> Enum.count(&validate/1) 
 end
