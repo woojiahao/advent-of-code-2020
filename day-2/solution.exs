89699f8e753ac518b314be2b3a77214395dcee33
woojiahao <woojiahao1234@gmail.com>
2020-12-05 11:59:10 +0800

    Add day 2 part 2 solution

--- day-2/solution.exs
+++ day-2/solution.exs
@@ -1,9 +1,21 @@
 defmodule Solution do
-  defp validate(input) do
-    [min, max, letter, password] = Regex.run(~r/^(\d+)-(\d+) (\w): (\w+)$/, input, capture: :all_but_first)
+  @data File.read!("data.txt") |> String.split("\n", trim: true)
+
+  defp split_input(input), do: Regex.run(~r/^(\d+)-(\d+) (\w): (\w+)$/, input, capture: :all_but_first)
+
+  defp validate_frequency(input) do
+    [min, max, letter, password] = split_input(input)
     freq = password |> String.graphemes() |> Enum.frequencies() |> Map.get(letter)
     freq >= String.to_integer(min) and freq <= String.to_integer(max) 
   end
 
-  def partOne(), do: File.read!("data.txt") |> String.split("\n", trim: true) |> Enum.count(&validate/1) 
+  defp validate_policy(input) do
+    [first, second, letter, password] = split_input(input)
+    first_letter = String.at(password, String.to_integer(first)-1)
+    second_letter = String.at(password, String.to_integer(second)-1)
+    (first_letter == letter or second_letter == letter) and first_letter != second_letter
+  end
+
+  def part_one(), do: @data |> Enum.count(&validate_frequency/1) 
+  def part_two(), do: @data |> Enum.count(&validate_policy/1)
 end
