c04c60e98c2ed4b61be7abe455b77bca38dbfd2f
woojiahao <woojiahao1234@gmail.com>
2020-12-05 18:20:03 +0800

    Add day 3 part 2 solution

--- day-3/solution.exs
+++ day-3/solution.exs
@@ -1,13 +1,30 @@
 defmodule Solution do
   @data File.read!("data.txt") |> String.split("\n", trim: true)
 
+  defp is_tree(row, i, right, down) do
+    raw_col = Stream.cycle(0..String.length(row)-1) |> Enum.take(div(i, down)*right) |> List.last()
+    col = if raw_col+1 == String.length(row), do: 0, else: raw_col+1
+    row |> String.graphemes() |> Enum.at(col) == "#"
+  end
+
+  defp traverse(rows, right, down) do
+    Enum.zip(1..length(rows), rows)
+      |> Enum.filter(fn {i, _} -> rem(i, down) == 0 end)
+      |> Enum.count(fn {i, row} -> is_tree(row, i, right, down) end)
+  end
+
   def part_one() do
     [_ | d] = @data
-    Enum.zip(1..(length(d)), d) 
-      |> Enum.count(fn {i, x} -> 
-        raw_col = Stream.cycle(0..String.length(x)-1) |> Enum.take(i*3) |> List.last()
-        col = if raw_col+1 == String.length(x), do: 0, else: raw_col+1
-        x |> String.graphemes() |> Enum.at(col) == "#" 
-      end)
+    traverse(d, 3, 1)
+  end
+
+  def part_two() do
+    [_ | d] = @data
+    s1 = traverse(d, 1, 1)
+    s2 = traverse(d, 3, 1)
+    s3 = traverse(d, 5, 1)
+    s4 = traverse(d, 7, 1)
+    s5 = traverse(d, 1, 2)
+    s1 * s2 * s3 * s4 * s5
   end
 end
