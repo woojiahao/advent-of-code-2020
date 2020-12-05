aafa1ccfeaf391836aab1fbd3543ee3764f2e235
woojiahao <woojiahao1234@gmail.com>
2020-12-05 21:13:29 +0800

    Refactor day 4 data loading
    Load data as map directly

--- day-4/solution.exs
+++ day-4/solution.exs
@@ -1,11 +1,20 @@
 defmodule Solution do
-  @data File.read!("data.txt") |> String.split("\n\n", trim: true)
+  @data File.read!("data.txt") 
+    |> String.split("\n\n", trim: true) 
+    |> Enum.map(fn x -> String.split(x, ~r{[\n|\s]}, trim: true) end) 
+    |> Enum.map(fn x -> Enum.map(x, fn y -> String.split(y, ":", trim: true) end) end)
+    |> Enum.map(fn x -> Map.new(x, fn [k, v] -> {k, v} end) end)
+
   @fields MapSet.new(["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"])
 
   def part_one() do
-    @data 
-      |> Enum.map(fn x -> String.split(x, ~r{[\n|\s]}) end) 
-      |> Enum.map(fn x -> x |> Enum.map(fn y -> String.split(y, ":") |> List.first() end) |> MapSet.new() end)
-      |> Enum.count(fn x -> MapSet.intersection(x, @fields) |> MapSet.size() == MapSet.size(@fields) end)
+    @data |> Enum.count(fn x -> 
+      MapSet.intersection(MapSet.new(Map.keys(x)), @fields) 
+      |> MapSet.size() == MapSet.size(@fields) 
+    end)
+  end
+
+  def part_two() do
+    nil
   end
 end
