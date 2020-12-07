ffdebeb836b36f27bb87be05dc350b8a2350f8f8
woojiahao <woojiahao1234@gmail.com>
2020-12-07 16:39:48 +0800

    Add day 6 part 2 solution

--- day-6/solution.exs
+++ day-6/solution.exs
@@ -1,5 +1,5 @@
 defmodule Solution do
-  def load_data(), do: File.read!("data.txt") |> String.split("\n\n", trim: true)
+  defp load_data(), do: File.read!("data.txt") |> String.split("\n\n", trim: true)
 
   def part_one() do
     load_data()
@@ -7,6 +7,19 @@ defmodule Solution do
     |> Enum.map(&String.graphemes/1)
     |> Enum.map(&Enum.uniq/1)
     |> Enum.map(&Enum.count/1)
+    |> Enum.sum()
+  end
+
+  def part_two() do
+    load_data()
+    |> Enum.map(&String.split(&1, "\n", trim: true))
+    |> Enum.map(fn answers ->
+      answers
+      |> Enum.flat_map(&String.graphemes/1)
+      |> Enum.frequencies()
+      |> Enum.filter(&(elem(&1, 1) == length(answers)))
+      |> length()
+    end)
     |> Enum.sum()
   end
 end
