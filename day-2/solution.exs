dd6b696ce6a5772a40a7a4e13275a17cec2759c3
woojiahao <woojiahao1234@gmail.com>
2020-12-10 11:45:54 +0800

    Refactor day 2 solutions

--- day-2/solution.exs
+++ day-2/solution.exs
@@ -1,20 +1,26 @@
 defmodule Solution do
-  @data File.read!("data.txt") |> String.split("\n", trim: true)
+  def load_data(),
+    do:
+      File.read!("data.txt")
+      |> String.split("\n", trim: true)
+      |> Enum.map(&split_input/1)
+      |> Enum.map(fn [x, y, letter, pass] ->
+        [String.to_integer(x), String.to_integer(y), letter, pass]
+      end)
 
-  defp split_input(input), do: Regex.run(~r/^(\d+)-(\d+) (\w): (\w+)$/, input, capture: :all_but_first)
+  defp split_input(input),
+    do: Regex.run(~r/^(\d+)-(\d+) (\w): (\w+)$/, input, capture: :all_but_first)
 
-  defp validate_frequency(input) do
-    [min, max, letter, password] = split_input(input)
+  defp validate_frequency([min, max, letter, password]) do
     freq = password |> String.graphemes() |> Enum.frequencies() |> Map.get(letter)
-    freq in String.to_integer(min)..String.to_integer(max)
+    freq in min..max
   end
 
-  defp validate_policy(input) do
-    [x, y, letter, password] = split_input(input)
-    [first, second] = [x, y] |> Enum.map(fn x -> String.at(password, String.to_integer(x)-1) end)
+  defp validate_policy([x, y, letter, password]) do
+    [first, second] = [x, y] |> Enum.map(&String.at(password, &1 - 1))
     (first == letter or second == letter) and first != second
   end
 
-  def part_one(), do: @data |> Enum.count(&validate_frequency/1) 
-  def part_two(), do: @data |> Enum.count(&validate_policy/1)
+  def part_one(), do: load_data() |> Enum.count(&validate_frequency/1)
+  def part_two(), do: load_data() |> Enum.count(&validate_policy/1)
 end
