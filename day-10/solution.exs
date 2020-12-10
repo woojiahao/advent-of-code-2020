42d7d06ec8c00ce01cb6bae13359f66476df63f7
woojiahao <woojiahao1234@gmail.com>
2020-12-10 23:00:39 +0800

    Complete day 10 part 2
    All hail memoization!!

--- day-10/solution.exs
+++ day-10/solution.exs
@@ -1,4 +1,6 @@
 defmodule Solution do
+  use Agent
+
   defp load_data() do
     # We can assume that there are no duplicates
     data =
@@ -6,21 +8,55 @@ defmodule Solution do
       |> String.split("\n", trim: true)
       |> Enum.map(&String.to_integer/1)
 
-    [0] ++ data ++ [(data |> Enum.max()) + 3]
+    ([0] ++ data ++ [(data |> Enum.max()) + 3]) |> Enum.sort()
   end
 
-  defp zip_with_next(lst) do
-    lst |> Enum.zip(lst |> Enum.slice(1, length(lst) - 1))
+  defp generate_tree(data) do
+    data
+    |> Enum.map(fn x ->
+      {x, data |> Enum.filter(&((&1 - x) in 1..3))}
+    end)
+    |> Map.new()
   end
 
-  def part_one() do
+  defp traverse(_tree, start, last) when start == last, do: 1
+
+  defp traverse(tree, start, last) do
+    IO.puts("#{start} -> #{inspect(tree[start], charlists: :as_lists)}")
+    cached_value = Agent.get(__MODULE__, &Map.get(&1, start))
+
+    if cached_value do
+      cached_value
+    else
+      paths =
+        tree[start]
+        |> Enum.map(&traverse(tree, &1, last))
+        |> List.flatten()
+        |> Enum.sum()
+
+      Agent.update(__MODULE__, &Map.put(&1, start, paths))
+
+      paths
+    end
+  end
+
+  defp part_one() do
     %{1 => n, 3 => m} =
       load_data()
-      |> Enum.sort()
-      |> zip_with_next()
-      |> Enum.map(fn {x, y} -> y - x end)
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.map(fn [x, y] -> y - x end)
       |> Enum.frequencies()
 
     n * m
+  end
+
+  def part_two() do
+    # To solve this part, we can map the outcomes as a tree
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+    data = load_data()
+
+    data
+    |> generate_tree()
+    |> traverse(0, data |> List.last())
   end
 end
