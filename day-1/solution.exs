daca065f794b8e0fe6371c4539a09f1c52573190
woojiahao <woojiahao1234@gmail.com>
2020-12-10 11:40:30 +0800

    Refactor day 1 solution

--- day-1/solution.exs
+++ day-1/solution.exs
@@ -1,15 +1,21 @@
-defmodule Solution do 
-  defp combinations(elems, size) when size == 2, do: for x <- elems, y <- elems, x != y, do: [x, y]
-  defp combinations(elems, size) when size == 3, do: for x <- elems, y <- elems, z <- elems, x != y, y != z, x != z, do: [x, y, z]
+defmodule Solution do
+  defp combinations(elems, size)
+       when size == 2,
+       do: for(x <- elems, y <- elems, x != y, do: [x, y])
+
+  defp combinations(elems, size)
+       when size == 3,
+       do: for(x <- elems, y <- elems, z <- elems, x != y, y != z, x != z, do: [x, y, z])
+
   defp solve(size \\ 2) do
     File.read!("data.txt")
-      |> String.split("\n", trim: true)
-      |> Enum.map(&String.to_integer/1)
-      |> combinations(size)
-      |> Enum.find(fn x -> Enum.sum(x) == 2020 end)
-      |> List.foldl(1, fn acc, x -> x * acc end)
-  end 
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> combinations(size)
+    |> Enum.find(&(Enum.sum(&1) == 2020))
+    |> List.foldl(1, &(&1 * &2))
+  end
 
-  def partOne(), do: solve()
-  def partTwo(), do: solve(3)
+  def part_one(), do: solve()
+  def part_two(), do: solve(3)
 end
