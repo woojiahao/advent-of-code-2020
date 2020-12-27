bda0981146d49e11b4de34ad1aa99de119c6bcb5
woojiahao <woojiahao1234@gmail.com>
2020-12-27 15:09:00 +0800

    Optimised day 15 part 2 to use map instead of list
    Map used will simple contain the index of the latest occurrence of the
    value

--- day-15/optimised.exs
+++ day-15/optimised.exs
@@ -1,36 +1,43 @@
 defmodule Optimised do
+  # Instead of storing all the values computed, store the latest positions of each value to save complexity
   defp load_data(),
     do:
-      File.read!("dummy.txt")
+      File.read!("data.txt")
       |> String.split("\n", trim: true)
       |> List.first()
       |> String.split(",", trim: true)
       |> Enum.map(&String.to_integer/1)
 
-  defp rfind(lst, v) do
-    [_ | rest] = lst |> Enum.filter(&(&1 != nil)) |> Enum.reverse()
+  defp gen_positions(data), do: data |> Stream.with_index() |> Map.new()
 
-    length(rest) - (rest |> Enum.find_index(&(&1 == v)))
-  end
+  defp solve(d, _cur, i, n) when i == n, do: d |> Enum.max_by(fn {v, j} -> j end) |> elem(0)
 
-  def solve(backlog, i, n) when i == n, do: backlog |> List.last()
-
-  def solve(backlog, i, n) do
-    IO.puts("i is #{i}")
-    prev = backlog |> Enum.at(i - 1)
-
-    elem =
-      if backlog |> Enum.count(&(&1 == prev)) == 1,
-        do: 0,
-        else: i - rfind(backlog, prev)
-
-    solve(backlog |> List.replace_at(i, elem), i + 1, n)
+  defp solve(d, cur, i, n) do
+    next = if Map.has_key?(d, cur), do: i - d[cur], else: 0
+    update = %{cur => i}
+    solve(Map.merge(d, update), next, i + 1, n)
   end
 
   def part_two() do
     data = load_data()
-    size = 30_000_000
-    lst = List.duplicate(nil, size - length(data))
-    solve(data ++ lst, length(data), size)
+    [last | rest] = data |> Enum.reverse()
+    solve(gen_positions(rest |> Enum.reverse()), last, length(rest), 30_000_000)
+
+    # solve(positions, {data |> List.last(), length(data) - 1}, 10)
+
+    # [
+    #   [0, 3, 6],
+    #   [1, 3, 2],
+    #   [2, 1, 3],
+    #   [1, 2, 3],
+    #   [2, 3, 1],
+    #   [3, 2, 1],
+    #   [3, 1, 2]
+    # ]
+    # |> Enum.map(fn d ->
+    #   [last | rest] = d |> Enum.reverse()
+    #   d! = rest |> Enum.reverse()
+    #   solve(gen_positions(d!), last, length(d!), 2020)
+    # end)
   end
 end
